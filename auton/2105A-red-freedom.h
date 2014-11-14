d9e660688e6be21edc27722dcb31beda7e8c7416
Kristaps Berzinch <kberzinch@gatech.edu>
2014-11-14 05:01:07 +0000

    Removed core testing (not needed) Blue freedom added Red autons added

---
+++ auton/2105A-red-freedom.h
@@ -0,0 +1,17 @@
+void Auton_Red_Freedom() {
+	Auton_Lift_Targeted(UP,1200);
+	Auton_Drive_TurnTo(CLOCKWISE,-350,63);
+	sleep(1000);
+	Auton_Drive_Targeted(FORWARD,80);
+	Auton_Lift_Targeted(DOWN,550);
+	Auton_Drive_Targeted(BACKWARD,-60);
+	Auton_Lift_Targeted(DOWN,100);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE,850);
+	Auton_Drive_Targeted(FORWARD,1500);
+	Auton_Lift_Targeted(UP,1800);
+	Auton_Drive_TurnTo(CLOCKWISE,200);
+	Auton_Drive_Targeted(FORWARD,25);
+	Auton_Lift_Targeted(DOWN,1400);
+	sleep(500);
+	Auton_Drive_Targeted(BACKWARD,-200);
+}
