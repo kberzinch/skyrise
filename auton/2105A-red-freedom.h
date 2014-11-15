83ef164ffe384cd35f89ab5f2604e7c529bd8ff0
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2014-11-15 03:23:07 +0000

    Changes during FLC (next commit is more current)

--- auton/2105A-red-freedom.h
+++ auton/2105A-red-freedom.h
@@ -4,9 +4,9 @@ void Auton_Red_Freedom() {
 	sleep(1000);
 	Auton_Drive_Targeted(FORWARD,80);
 	Auton_Lift_Targeted(DOWN,550);
-	Auton_Drive_Targeted(BACKWARD,-60);
+	//Auton_Drive_Targeted(BACKWARD,-60);
 	Auton_Lift_Targeted(DOWN,100);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE,850);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE,700);
 	Auton_Drive_Targeted(FORWARD,1500);
 	Auton_Lift_Targeted(UP,1800);
 	Auton_Drive_TurnTo(CLOCKWISE,200);
