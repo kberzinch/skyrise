0a6ccbc7d54b93f7904f02bad89d3539d783c458
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2014-12-02 20:24:09 +0000

    TODO: Watchdog timers Fixed presets (courtesy of Sultan)

--- auton/2105A-blue-atloader.h
+++ auton/2105A-blue-atloader.h
@@ -1,10 +1,29 @@
 void Auton_Blue_AtLoader() {
 	Auton_Drive_Targeted(LEFT,10);
-	Auton_Lift_Targeted(UP,450);
+	Auton_Lift_Targeted(UP,350);
 	sleep(500);
 	Auton_Drive_Targeted(FORWARD, 120);
 	sleep(500);
 	Auton_Lift_Targeted(UP,700);
-	Auton_Drive_Targeted(BACKWARD,400);
-	Auton_Drive_TurnTo(CLOCKWISE,-700);
+	Auton_Drive_Targeted(BACKWARD,160);
+	Auton_Drive_TurnTo(CLOCKWISE,-600);
+	Auton_Drive_Targeted(FORWARD,30);
+	sleep(500);
+	Auton_Lift_Targeted(DOWN,0, 63);
+	sleep(500);
+	Auton_Drive_Targeted(BACKWARD,50);
+	sleep(500);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE,-400);
+	sleep(500);
+	Auton_Lift_Targeted(UP,350);
+	sleep(500);
+	Auton_Drive_Targeted(FORWARD,200);
+	sleep(500);
+	Auton_Lift_Targeted(UP,700);
+	sleep(500);
+	Auton_Drive_Targeted(BACKWARD,160);
+	sleep(500);
+	Auton_Drive_TurnTo(CLOCKWISE,-600);
+	sleep(500);
+	Auton_Drive_Targeted(FORWARD,30);
 }
