19e9511264ebcff9800d10badffd37635febea76
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2014-12-04 22:24:31 +0000

    Added more info to autonmanager finishing message Sped up blue atloader auton

--- auton/2105A-blue-atloader.h
+++ auton/2105A-blue-atloader.h
@@ -1,43 +1,47 @@
+task LiftToAutoloader {
+	Auton_Lift_Targeted(UP,400);
+}
+
 void Auton_Blue_AtLoader() {
 	Auton_Drive_Targeted(LEFT,10);
 	Auton_Lift_Targeted(UP,400);
-	sleep(500);
+	sleep(250);
 	Auton_Drive_Targeted(FORWARD, 120);
-	sleep(500);
+	sleep(250);
 	Auton_Lift_Targeted(UP,700);
 	Auton_Drive_Targeted(BACKWARD,160);
 	Auton_Drive_TurnTo(CLOCKWISE,-600);
 	Auton_Drive_Targeted(FORWARD,90);
-	sleep(1000);
-	Auton_Lift_Targeted(DOWN,0, 63);
-	sleep(500);
+	sleep(250);
+	Auton_Lift_Targeted(DOWN,0,63);
+	sleep(100);
+	startTask(LiftToAutoloader);
 	Auton_Drive_Targeted(BACKWARD,70);
 	Auton_Drive_TurnTo(COUNTERCLOCKWISE,-160);
-	sleep(500);
-	Auton_Lift_Targeted(UP,400);
-	sleep(500);
+	sleep(250);
 	Auton_Drive_Targeted(FORWARD,200);
-	sleep(500);
+	// PICKED UP SECOND PYLON
+	sleep(250);
 	Auton_Lift_Targeted(UP,750);
-	sleep(500);
+	sleep(250);
 	Auton_Drive_Targeted(BACKWARD,160);
 	Auton_Drive_TurnTo(CLOCKWISE,-600);
 	Auton_Drive_Targeted(FORWARD,100);
-	sleep(500);
+	sleep(250);
 	Auton_Lift_Targeted(DOWN,200);
+	sleep(100);
+	startTask(LiftToAutoloader);
 	Auton_Drive_Targeted(BACKWARD,100);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE,-350);
-	sleep(500);
-	Auton_Lift_Targeted(UP,400);
-	sleep(500);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE,-160);
+	sleep(250);
 	Auton_Drive_Targeted(FORWARD,150);
-	sleep(500);
+	sleep(250);
 	Auton_Lift_Targeted(UP,1100);
-	sleep(500);
+	sleep(250);
 	Auton_Drive_Targeted(BACKWARD,160);
 	Auton_Drive_TurnTo(CLOCKWISE,-600);
 	Auton_Drive_Targeted(FORWARD,100);
-	sleep(1000);
-	Auton_Lift_Targeted(DOWN,200);
+	sleep(250);
+	Auton_Lift_Targeted(DOWN,700);
 	Auton_Drive_Targeted(BACKWARD,100);
 }
