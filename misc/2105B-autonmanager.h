edc3a3f27e3b8229266d261a0a170af59b58aa5d
msultan <msultan@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2015-01-08 20:53:32 +0000

    added the old 2105B code and the auton manager

---
+++ misc/2105B-autonmanager.h
@@ -0,0 +1,39 @@
+task autonomous {
+	/*if(Competition.Auton == 3) {
+		Auton_Progskills();
+		stopTask(autonomous);
+	}*/
+	switch(Competition.IsBlue) {
+	case true:
+		switch(Competition.AtLoader) {
+		case true:
+			Auton_Blue_AtLoader();
+			break;
+		case false:
+			//Auton_Blue_Freedom();
+			break;
+		}
+		break;
+	case false:
+		switch(Competition.AtLoader) {
+		case true:
+			//Auton_Red_AtLoader();
+			break;
+		case false:
+			//Auton_Red_Freedom();
+			break;
+		}
+		break;
+	}
+#if defined(_DEBUG)
+	writeDebugStreamLine("Autonomous finished");
+	writeDebugStreamLine(" - Total time: %i:%i",LCD_Timer_Mins(0),LCD_Timer_Secs(0,true));
+	writeDebugStreamLine(" - Batt A   %1.2fv", (float)nImmediateBatteryLevel / (float)1000);
+#ifndef NoPowerExpander
+	writeDebugStreamLine(" - Batt B   %1.2fv", (float)SensorValue[PowerExpander] / (float)280); // TBD: Verify magic number
+#endif
+	writeDebugStreamLine(" - Backup   %1.2fv", (float)BackupBatteryLevel / (float)1000);
+	LCD.Display.Paused = true;
+	ResetDriveEncoders();
+#endif
+}
