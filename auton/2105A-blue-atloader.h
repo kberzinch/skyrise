352588ab2428efe421567081aa9a966e368036f7
Kristaps Berzinch <kberzinch@gatech.edu>
2015-01-10 16:02:32 +0000

    [Savannah] 2105A tweaking autons

--- auton/2105A-blue-atloader.h
+++ auton/2105A-blue-atloader.h
@@ -3,7 +3,7 @@ task LiftToAutoloader {
 }
 
 void Auton_Blue_AtLoader() {
-	Auton_Drive_Targeted(LEFT,10);
+	//Auton_Drive_Targeted(LEFT,10);
 	Auton_Lift_Targeted(UP,400);
 	sleep(250);
 	Auton_Drive_Targeted(FORWARD, 120);
@@ -28,7 +28,7 @@ void Auton_Blue_AtLoader() {
 	Auton_Drive_TurnTo(CLOCKWISE,-600);
 	Auton_Drive_Targeted(FORWARD,100);
 	sleep(500);
-	Auton_Lift_Targeted(DOWN,200);
+	Auton_Lift_Targeted(DOWN,200,127,2000);
 	sleep(100);
 	startTask(LiftToAutoloader);
 	Auton_Drive_Targeted(BACKWARD,100);
