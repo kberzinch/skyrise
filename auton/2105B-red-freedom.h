45db00aa6ef7fc491f5d3a2d16b969dd42c6ef00
msultan <msultan@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2015-01-15 22:04:17 +0000

    Red auton for B bot kinda working

--- auton/2105B-red-freedom.h
+++ auton/2105B-red-freedom.h
@@ -1,38 +1,25 @@
 void Auton_Red_Freedom() {
-	Collection(-127);
-	sleep(400);
-	Collection();
-	Auton_Drive_Targeted(FORWARD,350);
-	Collection(127);
-	sleep(700);
-	Collection();
-	Auton_Drive_Targeted(BACKWARD,250);
-	Auton_Drive(CLOCKWISE);
-	ResetDriveEncoders();
-	while(SensorValue[DriveEncoder] > -90) {}
-	Auton_Drive();
-	Auton_Drive_Targeted(FORWARD,400);
-	Auton_Drive(COUNTERCLOCKWISE);
-	ResetDriveEncoders();
-	while(SensorValue[DriveEncoder] < 580) {}
-	Auton_Drive();
-	Lift_Target = 50;
 	startTask(Lift_Stabilizer_Left);
 	startTask(Lift_Stabilizer_Right);
-	sleep(1000);
-	Auton_Drive_Targeted(FORWARD,220);
+	Auton_Drive_Targeted(BACKWARD, 125);
 	Collection(-127);
-	sleep(500);
+	sleep(450);
 	Collection();
-	Auton_Drive_Targeted(BACKWARD,400);
-	Auton_Drive(COUNTERCLOCKWISE);
-	ResetDriveEncoders();
-	while(SensorValue[DriveEncoder] < 340) {}
-	Auton_Drive();
-	Lift_Target = 90;
-	Auton_Drive_Targeted(FORWARD,280); // increase probably
-	sleep(1000);
+	Lift_Target = 55; // low
+	while(SensorValue[EncoderLiftLeft] < 40){}
+	wait1Msec(500);
 	Collection(-127);
-	sleep(1000);
-	Auton_Drive(BACKWARD,127,500);
+	sleep(550);
+	Collection();
+	Auton_Drive_Targeted(BACKWARD, 600);
+	Lift_Target = 0; // low
+	while(SensorValue[EncoderLiftLeft] > 10){}
+	wait1Msec(500);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 390);
+	Collection(-127);
+	sleep(700);
+	Collection();
+	Auton_Drive_Targeted(FORWARD, 200);
+
+	//Auton_Drive_Targeted(FORWARD, 100);
 }
