577273a8662b7687b8b8f35d07df8c3af30e2d64
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2015-01-20 18:07:12 +0000

    2105A: Added AlignBarButton, removed other solenoid Cleaned up Auton_Red_AtLoader to prep for redo Drive_Targeted now avgs both drive encoders, if available. Only works forwards and backwards. Menu shorted to 1 level (color) Cleaned up autonmanager for ^

--- misc/2105A-autonmanager.h
+++ misc/2105A-autonmanager.h
@@ -1,39 +1,15 @@
 task autonomous {
 	/*if(Competition.Auton == 3) {
-		Auton_Progskills();
-		stopTask(autonomous);
+	Auton_Progskills();
+	stopTask(autonomous);
 	}*/
 	switch(Competition.IsBlue) {
 	case true:
-		switch(Competition.AtLoader) {
-		case true:
-			Auton_Blue_AtLoader();
-			break;
-		case false:
-			//Auton_Blue_Freedom();
-			break;
-		}
+		Auton_Blue_AtLoader();
 		break;
 	case false:
-		switch(Competition.AtLoader) {
-		case true:
-			Auton_Red_AtLoader();
-			break;
-		case false:
-			//Auton_Red_Freedom();
-			break;
-		}
+		Auton_Red_AtLoader();
 		break;
 	}
-#if defined(_DEBUG)
-	writeDebugStreamLine("Autonomous finished");
-	writeDebugStreamLine(" - Total time: %i:%i",LCD_Timer_Mins(0),LCD_Timer_Secs(0,true));
-	writeDebugStreamLine(" - Batt A   %1.2fv", (float)nImmediateBatteryLevel / (float)1000);
-#ifndef NoPowerExpander
-	writeDebugStreamLine(" - Batt B   %1.2fv", (float)SensorValue[PowerExpander] / (float)280); // TBD: Verify magic number
-#endif
-	writeDebugStreamLine(" - Backup   %1.2fv", (float)BackupBatteryLevel / (float)1000);
-	LCD.Display.Paused = true;
-	ResetDriveEncoders();
-#endif
+	AutonDataDump();
 }
