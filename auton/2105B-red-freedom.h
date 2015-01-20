c919306b0f1927bd81c02d044d6933f25c3e8eb3
Mureed Sultan <sultan.0313@gmail.com>
2015-01-20 23:16:29 +0000

    3 pts auton for 2105B

--- auton/2105B-red-freedom.h
+++ auton/2105B-red-freedom.h
@@ -4,33 +4,33 @@ void Auton_Red_Freedom() {
 	Auton_Drive_Targeted(BACKWARD, 100);
 	Collection(200);
 	Lift_Target = 45; // low post
-	while(SensorValue[EncoderLiftLeft] < 30) {}
+	while(SensorValue[EncoderLiftLeft] < 30){}
 	Collection(600);
 	Auton_Drive_Targeted(BACKWARD, 300);
 	Lift_Target = 0;
-	while(SensorValue[EncoderLiftLeft] > 10){}
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 400);
-	Auton_Drive_Targeted(FORWARD, 2);
+	while(SensorValue[EncoderLiftLeft] > 10){} // LIFT DOWN AFTER 1st cube
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 395); // 2nd cube turn
+	Auton_Drive_Targeted(FORWARD, 50);
 	Collection(200);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 650);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 650); // TURN TOWARDS 2nd Pole
 	Lift_Target = 80;
 	while(SensorValue[EncoderLiftLeft] < 70) {}
 	Auton_Drive_Targeted(FORWARD,20);
 	Collection(600);
 	Auton_Drive_Targeted(BACKWARD,100);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 1300);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 1290); // WAS 1300
 	Lift_Target = 0;
-	Collection(1000);
-	Auton_Drive_Targeted(FORWARD,1200,127,4000);
-	Auton_Drive_TurnTo(CLOCKWISE,700);
-	Auton_Drive_Targeted(FORWARD,70);
-	Collection(200);
-	Auton_Drive_Targeted(BACKWARD,70);
-	Lift_Target = 45;
+	Collection(1100);
+	Auton_Drive_Targeted(FORWARD,1270,127,4000); // AFTER 2nd cube
+	Auton_Drive_TurnTo(CLOCKWISE,750);
+	Auton_Drive_Targeted(FORWARD,65);
+	Collection(700);
+	//Auton_Drive_Targeted(BACKWARD,80);
+	Lift_Target = 55;
 	while(SensorValue[EncoderLiftLeft] < 35) {}
-	sleep(500);
-	Auton_Drive_TurnTo(CLOCKWISE,700);
-	Auton_Drive_Targeted(FORWARD,50);
-	Collection(600);
-	Auton_Drive_Targeted(BACKWARD,100);
+	//sleep(500);
+	//Auton_Drive_TurnTo(CLOCKWISE,960);
+	Auton_Drive_Targeted(FORWARD,15);
+	Collection(900);
+	Auton_Drive_Targeted(BACKWARD,300);
 }
