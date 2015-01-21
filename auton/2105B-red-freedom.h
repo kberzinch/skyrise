d1784151d502a2c8e9082d0791224257656d9f65
Kristaps Berzinch <kberzinch@gatech.edu>
2015-01-21 17:19:45 +0000

    Fixing sultan's messes

--- auton/2105B-red-freedom.h
+++ auton/2105B-red-freedom.h
@@ -1,18 +1,18 @@
 void Auton_Red_Freedom() {
 	startTask(Lift_Stabilizer_Left);
 	startTask(Lift_Stabilizer_Right);
-	Auton_Drive_Targeted(BACKWARD, 100);
+	Auton_Drive_Targeted(BACKWARD, 85);
 	Collection(200);
 	Lift_Target = 45; // low post
 	while(SensorValue[EncoderLiftLeft] < 30){}
 	Collection(600);
-	Auton_Drive_Targeted(BACKWARD, 300);
+	Auton_Drive_Targeted(BACKWARD, 350);
 	Lift_Target = 0;
-	while(SensorValue[EncoderLiftLeft] > 10){} // LIFT DOWN AFTER 1st cube
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 395); // 2nd cube turn
+	while(SensorValue[EncoderLiftLeft] > 10) {} // LIFT DOWN AFTER 1st cube
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 350); // 2nd cube turn
 	Auton_Drive_Targeted(FORWARD, 50);
 	Collection(200);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 650); // TURN TOWARDS 2nd Pole
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 625); // TURN TOWARDS 2nd Pole
 	Lift_Target = 80;
 	while(SensorValue[EncoderLiftLeft] < 70) {}
 	Auton_Drive_Targeted(FORWARD,20);
@@ -22,15 +22,13 @@ void Auton_Red_Freedom() {
 	Lift_Target = 0;
 	Collection(1100);
 	Auton_Drive_Targeted(FORWARD,1270,127,4000); // AFTER 2nd cube
-	Auton_Drive_TurnTo(CLOCKWISE,750);
+	Auton_Drive_TurnTo(CLOCKWISE,700); // whacko
 	Auton_Drive_Targeted(FORWARD,65);
 	Collection(700);
-	//Auton_Drive_Targeted(BACKWARD,80);
+	Auton_Drive_Targeted(BACKWARD,80);
 	Lift_Target = 55;
 	while(SensorValue[EncoderLiftLeft] < 35) {}
-	//sleep(500);
-	//Auton_Drive_TurnTo(CLOCKWISE,960);
-	Auton_Drive_Targeted(FORWARD,15);
+	Auton_Drive_Targeted(FORWARD,50);
 	Collection(900);
 	Auton_Drive_Targeted(BACKWARD,300);
 }
