a7a2fcdb519a2bf6c2ace9ca4cb64f533b6025d3
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2015-02-09 23:08:56 +0000

    2150b red freedom

--- auton/2105B-red-freedom.h
+++ auton/2105B-red-freedom.h
@@ -1,36 +1,44 @@
 void Auton_Red_Freedom() {
 	startTask(Lift_Stabilizer_Left);
 	startTask(Lift_Stabilizer_Right);
-	Auton_Drive_Targeted(BACKWARD, 85);
+	Auton_Drive_Targeted_PID(BACKWARD, -125);
 	Collection(200);
-	Lift_Target = 45; // low post
-	while(SensorValue[EncoderLiftLeft] < 30){}
+	Lift_Target = 320; // low post
+	MinOverride = true;
+	while(SensorValue[EncoderLiftLeft] < 310) {}
+	MinOverride = false;
 	Collection(600);
-	Auton_Drive_Targeted(BACKWARD, 350);
+	Auton_Drive_Targeted_PID(BACKWARD, -500);
 	Lift_Target = 0;
+	MinOverride = true;
 	while(SensorValue[EncoderLiftLeft] > 10) {} // LIFT DOWN AFTER 1st cube
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 380); // 2nd cube turn
-	Auton_Drive_Targeted(FORWARD, 25);
+	MinOverride = false;
+	Auton_Drive_TurnTo_PID(COUNTERCLOCKWISE, 450, 127, 5, 1000); // 2nd cube turn
+	Auton_Drive_Targeted(FORWARD, -25); // this doesn't do anything
 	Collection(200);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 630); // TURN TOWARDS 2nd Pole
-	Lift_Target = 80;
-	while(SensorValue[EncoderLiftLeft] < 70) {}
-	Auton_Drive_Targeted(FORWARD,15);
+	Auton_Drive_TurnTo_PID(COUNTERCLOCKWISE, 630); // TURN TOWARDS 2nd Pole
+	Lift_Target = 640;
+	MinOverride = true;
+	while(SensorValue[EncoderLiftLeft] < 630) {}
+	MinOverride = false;
+	Auton_Drive_Targeted_PID(FORWARD,-15);
 	Collection(600);
-	Auton_Drive_Targeted(BACKWARD,100);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 1290); // WAS 1290
+	Auton_Drive_Targeted_PID(BACKWARD,100);
+	Auton_Drive_TurnTo_PID(COUNTERCLOCKWISE, 1290); // WAS 1290
 	Lift_Target = 0;
 	Collection(1100);
 	sleep(100);
-	Auton_Drive_Targeted(FORWARD,1230,127,4000); // AFTER 2nd cube
-	Auton_Drive_TurnTo(CLOCKWISE,652); // whacko 700
-	Auton_Drive_Targeted(FORWARD,65);
+	Auton_Drive_Targeted_PID(FORWARD,1230,127,4000); // AFTER 2nd cube
+	Auton_Drive_TurnTo_PID(CLOCKWISE,652); // whacko 700
+	Auton_Drive_Targeted_PID(FORWARD,65);
 	Collection(700);
-	Auton_Drive_Targeted(BACKWARD,80);
-		Lift_Target = 45;
-	while(SensorValue[EncoderLiftLeft] < 35) {}
-	Auton_Drive_Targeted(FORWARD,93);
+	Auton_Drive_Targeted_PID(BACKWARD,80);
+	Lift_Target = 360;
+	MinOverride = true;
+	while(SensorValue[EncoderLiftLeft] < 350) {}
+	MinOverride = false;
+	Auton_Drive_Targeted_PID(FORWARD,93);
 	Collection(900);
 	writeDebugStreamLine("Dropped cube at %i:%2i.%3i",LCD_Timer_Mins(0),LCD_Timer_Secs(0,true),LCD_Timer_Msecs(0,true));
-	Auton_Drive_Targeted(BACKWARD,300);
+	Auton_Drive_Targeted_PID(BACKWARD,300);
 }
