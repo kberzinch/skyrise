264588578a9145593a277ee7f3c43c766b4009a7
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2015-02-27 13:51:58 +0000

    Perfect?

--- auton/2105A-red-atloader-legacy.h
+++ auton/2105A-red-atloader-legacy.h
@@ -3,14 +3,14 @@ void Auton_Red_AtLoader() {
 	long Watchdog;
 	Auton_Lift_Targeted(UP, 725);
 	Auton_Drive_Targeted(FORWARD, 50);
-	Auton_Drive_TurnTo(CLOCKWISE, -650);
+	Auton_Drive_TurnTo(CLOCKWISE, -600);
 	Auton_Drive_Targeted(BACKWARD, 80);
 	sleep(500);
 	Auton_Lift_Targeted(DOWN, 450);
 	// START FIRST PYLON
 	sleep(500);
 	Claw(CLOSE);
-	Auton_Lift_Targeted(UP, 550 + lift);
+	Auton_Lift_Targeted(UP, 600 + lift);
 	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 900);
 	Auton_Drive(FORWARD);
 	Watchdog = nSysTime;
