5f6ace14f08ae0c06c9d42e2ad18af61cadab098
Mureed Sultan <sultan.0313@gmail.com>
2015-03-16 21:26:59 +0000

    new prog skill thing

--- auton/2105B-ProgrammingSkills.h
+++ auton/2105B-ProgrammingSkills.h
@@ -2,12 +2,32 @@ void Auton_Progskills() {
 	startTask(Lift_Stabilizer_Left);
 	startTask(Lift_Stabilizer_Right);
 	Collection(200);
-	Auton_Drive_Targeted(FORWARD, 80, 80);
-	writeDebugStreamLine("%i",SensorValue[DriveEncoder]);
-	while(SensorValue[DriveEncoder] < 125) {}
-	writeDebugStreamLine("%i",SensorValue[DriveEncoder]);
+	Auton_Drive_Targeted_PID(FORWARD, -125);
 	Collection(-150);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 140);
+	Auton_WaitForKeyPress(250);
+	Auton_Drive_Targeted(BACKWARD, 150);
+	Lift_Target = 340;
+	MinOverride = true;
+	long StartTime = nSysTime;
+	while(((SensorValue[EncoderLiftLeft] + SensorValue[EncoderLiftRight]) / 2) < 230 && (nSysTime - StartTime) < 1000) {}
+	if((nSysTime - StartTime) > 1000)
+		writeDebugStreamLine("timed out");
+	MinOverride = false;
+	Auton_Drive_Targeted(FORWARD, 20);
+	Collection(600);
+	Auton_Drive_Targeted(BACKWARD, 100);
+	Collection(350);
+	Lift_Target = 20;
+	Auton_WaitForKeyPress(250);
+	Auton_Drive_Targeted_PID(FORWARD, -125);
+	Collection(-300);
+	Auton_Drive_TurnTo_PID(CLOCKWISE, 2850);
+	Auton_Drive_Targeted(FORWARD, 750);
+	Collection(-600);
+	return;
+
+	/*
+	Auton_Drive_TurnTo(CLOCKWISE, -250); // Needs to be lower
 	Lift_Target = 575;
 	MinOverride = true;
 	long StartTime = nSysTime;
@@ -15,38 +35,31 @@ void Auton_Progskills() {
 	if((nSysTime - StartTime) > 2000)
 		writeDebugStreamLine("timed out");
 	MinOverride = false;
-	sleep(100);
-	Auton_Drive_Targeted_PID(FORWARD, -100);
-	sleep(100);
+	Auton_Drive_Targeted(FORWARD, 90);
 	Collection(800);
-	Auton_Drive_Targeted(BACKWARD, 150);
-	Auton_Drive_TurnTo(CLOCKWISE, -50); // reduced for battery?(50)
+	Auton_Drive_Targeted(BACKWARD, 130); //170
+	Collection(1000);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 20);
 	Lift_Target = 20; // Lift down
 	MinOverride = true;
+	Auton_Drive_TurnTo_PID(COUNTERCLOCKWISE, 1025, 127, 5, 1000);
 	while(SensorValue[EncoderLiftLeft] > 30) {}
 	MinOverride = false;
-	Collection(1000);
-	Auton_WaitForKeyPress(2);
-	Auton_Drive_Targeted_PID(FORWARD,-130);
+	Auton_Drive_Targeted(FORWARD, 380);
+	sleep(250);
 	Collection(700);
-Auton_Drive_TurnTo(COUNTERCLOCKWISE,-770);//-760
-Auton_Drive_Targeted_PID(FORWARD,-350);//-350
-Collection(300);
-Auton_Drive_Targeted_PID(BACKWARD,-325);
-Auton_WaitForKeyPress(1000);
-Auton_Drive_Targeted_PID(BACKWARD,-200);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 1250); // Needs to be higher
+	Auton_Drive_Targeted(FORWARD, 5);
+	Collection(300);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 2375); // Lower
+	Auton_Drive_Targeted(FORWARD, 50);
 	Lift_Target = 350;
 	MinOverride = true;
-	StartTime = nSysTime;
-	while(((SensorValue[EncoderLiftLeft] + SensorValue[EncoderLiftRight]) / 2) < 565 && (nSysTime - StartTime) < 2000) {}
-	if((nSysTime - StartTime) > 2000)
-		writeDebugStreamLine("timed out");
+	while(((SensorValue[EncoderLiftLeft] + SensorValue[EncoderLiftRight]) / 2) < 340) {}
 	MinOverride = false;
-	Auton_Drive_Targeted_PID(FORWARD,-50);
-	Collection(1500);
-	sleep(500);
-	Auton_Drive_Targeted(BACKWARD,400);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE,60);
-	Auton_Drive_Targeted(BACKWARD,20000);
-
+	Auton_Drive_Targeted(FORWARD, 50);
+	Collection(1000);
+	writeDebugStreamLine("Cubes scored: %i:%2i.%-3i",LCD_Timer_Mins(0),LCD_Timer_Secs(0,true),LCD_Timer_Msecs(0,true));
+	Auton_Drive_Targeted(BACKWARD, 50);
+	*/
 }
