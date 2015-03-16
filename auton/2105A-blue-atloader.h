e96e2cf239e85575ef34f3291d72200cab8a2d99
Kristaps Berzinch <kberzinch@gatech.edu>
2015-03-16 20:13:27 +0000

    Developed blue atloader auton Programming skills works ok

--- auton/2105A-blue-atloader.h
+++ auton/2105A-blue-atloader.h
@@ -3,47 +3,30 @@ void Auton_Blue_AtLoader() {
 	long Watchdog;
 	Auton_Lift_Targeted(UP, 725);
 	Auton_Drive_Targeted(FORWARD, 50);
-	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 600);
+	Auton_Drive_TurnTo(COUNTERCLOCKWISE, 650);
 	Auton_Drive_Targeted(BACKWARD, 30);
-	//Auton_WaitForKeyPress();
 	sleep(1000);
 	Auton_Lift_Targeted(DOWN, 450);
-	for(int pylon = 1; pylon < 6; pylon++) {
+	for(int pylon = 1; pylon < 3; pylon++) {
 		Auton_Drive_Targeted(FORWARD, 20, 127, 500);
 		sleep(250);
+		writeDebugStreamLine("!!!%i",SensorValue[SolenoidA]);
 		Claw(CLOSE);
+		writeDebugStreamLine("!!!%i",SensorValue[SolenoidA]);
 		Auton_Lift_Targeted(UP, 700 + lift);
-		if(pylon != 5) {
-			Auton_Drive_TurnTo(CLOCKWISE, -470, 63);
-			sleep(500);
-			} else {
-			Auton_WaitForKeyPress();
-		}
+		Auton_Drive_TurnTo(CLOCKWISE, -650, 63);
 		Auton_Drive(FORWARD);
 		Watchdog = nSysTime;
 		while(SensorValue[AlignButton] == 0 && nSysTime - Watchdog < 1000) {}
 		Auton_Drive();
-		sleep(750 + lift);
-		if(pylon > 3)
-			Auton_Lift_Targeted(DOWN,0 + lift, 63);
-		else
-			Auton_Lift_Targeted(DOWN,0 + lift);
+		Auton_Lift_Targeted(DOWN,100 + lift, 50);
 		Claw(OPEN);
 		Auton_Drive_Targeted(BACKWARD,40);
-		if(pylon == 5)
-			return;
 		Auton_Lift_Targeted(UP,650);
-		Auton_Drive_TurnTo(COUNTERCLOCKWISE, 750, 63);
-		if(pylon > 3)
-			Auton_Drive_TurnTo(COUNTERCLOCKWISE, 770, 63);
+		Auton_Drive_TurnTo(COUNTERCLOCKWISE, 650, 63);
 		Auton_Drive_Targeted(BACKWARD, 30);
-		sleep(1000 + lift);
 		Auton_Lift_Targeted(DOWN,450);
-		lift += 250;
-		//Auton_WaitForKeyPress();
+		lift += 200;
 		sleep(500);
-#if defined(_DEBUG)
-		AutonDataDump();
-#endif
 	}
 }
