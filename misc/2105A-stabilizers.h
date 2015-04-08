c9bfcbbec20e2f7760b2c5312d5eb33d0f1a658f
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2015-04-08 00:56:11 +0000

    Changing things

--- misc/2105A-stabilizers.h
+++ misc/2105A-stabilizers.h
@@ -1,9 +1,17 @@
 int Lift_Target = 0;
 bool IsStabilizerRunning = false;
-float Kp = 0.15;
-float Ki = 0.00001;
-const int IntegralLimit = 100;
-float Kd = 0.02;
+const float Kp = 1.2;
+const float Kd = 1.5;
+const float Ki = 0;//.0001;
+const tSensors sensorLeft = I2C_4;
+const tSensors sensorRight = I2C_3;
+const int leftAdjust = 0; // -350;
+const int rightAdjust = 0; // -430;
+const int leftMultiplier = -1;
+const int rightMultiplier = 1;
+const int IntegralLimit = 200;
+const int Max = 1400;
+const int Min = -20;
 bool MinOverride = false;
 
 int Normalize(int speed) {
@@ -16,56 +24,59 @@ int Normalize(int speed) {
 	return speed;
 }
 
+int divisible(int num) {
+	if(num <= 0) {
+		return 1;
+	}
+	return num;
+}
+
+#define PID_SEQUENCE\
+IsStabilizerRunning = true;\
+int error = 0, previous_error = 0, speed = 0, derivative = 0, integral = 0;\
+int last_nSysTime = nSysTime;\
+while(IsStabilizerRunning) {\
+	if(((mult * SensorValue[sensor]) + offset) > Max || (((mult * SensorValue[sensor]) + offset) < Min && !MinOverride)) {\
+		motor[motor1] = 0;\
+		motor[motor2] = 0;\
+		error = 0;\
+		integral = 0;\
+		derivative = 0;\
+		previous_error = 0;\
+		continue;\
+	}\
+	error = (Lift_Target - ((mult * SensorValue[sensor]) + offset));\
+	if(abs(error) < IntegralLimit) {\
+		integral += error / divisible(nSysTime - last_nSysTime);\
+		} else {\
+		integral = 0;\
+	}\
+	derivative = (error - previous_error) / divisible(nSysTime - last_nSysTime);\
+	last_nSysTime = nSysTime;\
+	previous_error = error;\
+	speed = Kp * error + Kd * derivative + Ki * integral;\
+	motor[motor1] = Normalize(speed);\
+	motor[motor2] = Normalize(speed);\
+}
+
 void Set_Lift_Target(int Adjustment = 0) {
-	Lift_Target = ((SensorValue[EncoderRight] + (SensorValue[EncoderLeft])) / 2) + Adjustment;
+	Lift_Target = ((rightMultiplier * (SensorValue[sensorRight] + rightAdjust) + leftMultiplier * (SensorValue[sensorLeft] + leftAdjust)) / 2) + Adjustment;
 }
 
 task Lift_Stabilizer_Left {
-	IsStabilizerRunning = true;
-	const tSensors sensor = EncoderLeft;
-	int error, previous_error, speed, derivative, integral = 0;
-	while(IsStabilizerRunning) {
-		if(SensorValue[sensor] > 105 || (SensorValue[sensor] < 10 && !MinOverride)) {
-			motor[LiftTopLeft] = 0;
-			motor[LiftBottomLeft] = 0;
-			continue;
-		}
-		error = (Lift_Target - (SensorValue[sensor]));
-		if(abs(error) < IntegralLimit) {
-			integral += error;
-			} else {
-			integral = 0;
-		}
-		derivative = error - previous_error;
-		previous_error = error;
-		speed = Kp * error + Kd * derivative + Ki * integral;
-		motor[LiftTopLeft] = Normalize(speed);
-		motor[LiftBottomLeft] = Normalize(speed);
-		EndTimeSlice();
-	}
+	const tSensors sensor = sensorLeft;
+	const tMotor motor1 = LiftTopLeft;
+	const tMotor motor2 = LiftBottomLeft;
+	const int offset = leftAdjust;
+	const int mult = leftMultiplier;
+	PID_SEQUENCE
 }
 
 task Lift_Stabilizer_Right {
-	IsStabilizerRunning = true;
-	const tSensors sensor = EncoderRight;
-	int error, previous_error, speed, derivative, integral = 0;
-	while(IsStabilizerRunning) {
-		if(SensorValue[sensor] > 105 || (SensorValue[sensor] < 10 && !MinOverride)) {
-			motor[LiftTopRight] = 0;
-			motor[LiftBottomRight] = 0;
-			continue;
-		}
-		error = (Lift_Target - SensorValue[sensor]);
-		if(abs(error) < IntegralLimit) {
-			integral += error;
-			} else {
-			integral = 0;
-		}
-		derivative = error - previous_error;
-		previous_error = error;
-		speed = Kp * error + Kd * derivative + Ki * integral;
-		motor[LiftTopRight] = Normalize(speed);
-		motor[LiftBottomRight] = Normalize(speed);
-		EndTimeSlice();
-	}
+	const tSensors sensor = sensorRight;
+	const tMotor motor1 = LiftTopRight;
+	const tMotor motor2 = LiftBottomRight;
+	const int offset = rightAdjust;
+	const int mult = rightMultiplier;
+	PID_SEQUENCE
 }
