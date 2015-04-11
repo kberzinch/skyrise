eab5a782f2d897d8f73ef5779943f6515ab993a4
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2015-04-11 16:26:32 +0000

    Removed complex pylon mechanism Developing red-freedom auton Even lower standards for battery indicators core.h and john usercontrol now work with two center motors Improved debugging for Auton_Lift_Targeted

--- auton/2105A-red-freedom.h
+++ auton/2105A-red-freedom.h
@@ -3,30 +3,36 @@ void Auton_Red_Freedom() {
 	// Cube in lower position
 	ResetDriveEncoders();
 	Auton_Drive(RIGHT);
-	while(nMotorEncoder[DriveCenter] < 50) {}
+	while(nMotorEncoder[DriveCenterA] > -100) {}
 	Auton_Drive();
 	ResetDriveEncoders();
-	Auton_Lift_Targeted(UP,200);
-	Set_Lift_Target();
+	Lift_Target = 400;
+	Auton_Lift_Targeted(UP,300);
 	startTask(Lift_Stabilizer_Left);
 	startTask(Lift_Stabilizer_Right);
-	Auton_Drive_Targeted(FORWARD, 400);
+	Auton_Drive_Targeted(FORWARD, 330);
 	sleep(500);
-	Lift_Target = -100;
-	while(((SensorValue[I2C_3] - SensorValue[I2C_4]) / 2) > -75) {}
-	Lift_Target = 970;
-	while(((SensorValue[I2C_3] - SensorValue[I2C_4]) / 2) < 750) {}
-	ResetDriveEncoders();
+	IsStabilizerRunning = false;
+	Auton_Lift_Targeted(DOWN, 0);
+	/*Lift_Target = -100;
+	while(((SensorValue[I2C_3] - SensorValue[I2C_4]) / 2) > -50) {}*/
+	sleep(250);
+	Lift_Target = 150;
+	startTask(Lift_Stabilizer_Left);
+	startTask(Lift_Stabilizer_Right);
+	while(((SensorValue[I2C_3] - SensorValue[I2C_4]) / 2) < 100) {}
 	Auton_Drive_TurnTo(COUNTERCLOCKWISE, -25);
 	ResetDriveEncoders();
 	Auton_Drive(LEFT);
-	while(nMotorEncoder[DriveCenter] > -100) {}
+	while(SensorValue[I2C_1] < 400) {}
 	Auton_Drive();
 	ResetDriveEncoders();
-	Auton_Drive_Targeted(FORWARD, 400);
+	Auton_Drive_Targeted(FORWARD, 450);
+	Lift_Target = 1100;
+	while(((SensorValue[I2C_3] - SensorValue[I2C_4]) / 2) < 1000) {}
 	sleep(500);
-	Auton_Drive_TurnTo(CLOCKWISE, -750);
-	Auton_Drive_Targeted(FORWARD, 50);
+	Auton_Drive_TurnTo(CLOCKWISE, -800, 63);
+	Auton_Drive_Targeted(FORWARD, 80);
 	sleep(500);
 	SensorValue[SolenoidCubes] = 0;
 	sleep(500);
