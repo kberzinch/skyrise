0c36f98b3bb9584e20a45fa3d0d5782a397ee251
kberzinch <kberzinch@a68237d6-c7f9-421a-9d52-f0b1deeb5325>
2015-04-15 03:49:47 +0000

    Developing autoloader autons (mostly work) BatteryIndicators now blinks if batteries are exceptionally low Core now asks repeatedly if you put air in it (you did, right?) John usercontrol no longer twerks

--- core/v3/batteryindicators.h
+++ core/v3/batteryindicators.h
@@ -1,17 +1,34 @@
 #define BatteryIndicators
 
+long battlast_nSysTime = nSysTime;
+long batt1last_nSysTime = nSysTime;
+
 task BatteryIndicate {
 	while(true) {
-		if(((float)BackupBatteryLevel / (float)1000) < 5) {
-			SensorValue[BackupBattLEDRed] = 1;
-			SensorValue[BackupBattLEDGreen] = 0;
+		if(((float)BackupBatteryLevel / (float)1000) < 8) {
+			if(((float)BackupBatteryLevel / (float)1000) < 2) {
+				if(nSysTime - battlast_nSysTime > 100) {
+					battlast_nSysTime = nSysTime;
+					SensorValue[BackupBattLEDRed] = !SensorValue[BackupBattLEDRed];
+				}
+				} else {
+				SensorValue[BackupBattLEDRed] = 1;
+				SensorValue[BackupBattLEDGreen] = 0;
+			}
 			} else {
 			SensorValue[BackupBattLEDRed] = 0;
 			SensorValue[BackupBattLEDGreen] = 1;
 		}
-		if(((float)nImmediateBatteryLevel / (float)1000) < 7.8) {
-			SensorValue[MainBattLEDRed] = 1;
-			SensorValue[MainBattLEDGreen] = 0;
+		if(((float)nImmediateBatteryLevel / (float)1000) < 8) {
+		if(((float)nImmediateBatteryLevel / (float)1000) < 7.5) {
+				if(nSysTime - batt1last_nSysTime > 100) {
+					batt1last_nSysTime = nSysTime;
+					SensorValue[MainBattLEDRed] = !SensorValue[MainBattLEDRed];
+				}
+				} else {
+				SensorValue[MainBattLEDRed] = 1;
+				SensorValue[MainBattLEDGreen] = 0;
+			}
 			} else {
 			SensorValue[MainBattLEDRed] = 0;
 			SensorValue[MainBattLEDGreen] = 1;
